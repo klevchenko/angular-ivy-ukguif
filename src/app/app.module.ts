f4c6e62b2a3e02cf17f85d2053b7bf1166a9cb81
Kostiantyn Levchenko <kostiantyn.levchenko@boosta.co>
2021-08-14 21:06:52 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { OfferComponent } from './offer/offer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { GeneralComponent } from './offer/general/general.component';
+import { ExpensesComponent } from './offer/expenses/expenses.component';
+import { IncomeComponent } from './offer/income/income.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {MatTableModule} from "@angular/material/table";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MaterialFileInputModule} from "ngx-material-file-input";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    OfferComponent,
+    GeneralComponent,
+    ExpensesComponent,
+    IncomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatOptionModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MaterialFileInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
